1543267de30eb1de9b17e95a501acfcba60340a0
Sagor Roy <sagorroy4778@gmail.com>
2026-04-30 12:08:58 +0600

    searchbar setup

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import Container from './layouts/Container'
 import Logo from '../assets/Logo.webp'
+import { RiSearchLine } from "react-icons/ri";
 
 const SearchBar = () => {
   return (
     <Container>
       <div className='flex justify-between'>
-        <div className='w-40 h-17'>
+        <div className='flex justify-center items-center'>
           <img 
             src={Logo} 
             alt="logo" 
             fetchpriority="high"
           />
         </div>
-        <div>sdfsdf</div>
+        <div>
+          <input 
+            className='border border-[#808080] w-[498px] py-3 pl-11 placeholder:text-[#808080] placeholder:text-sm placeholder:font-pop rounded-md' 
+            type="text"  
+            placeholder='Search' 
+          />
+          <RiSearchLine className='relative -top-8 left-4' />
+        </div>
         <div>sdfsdf</div>
       </div>
     </Container>
