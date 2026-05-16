06352f4697bca45911b12bdebd4ffcaf3df52e72
Sagor Roy <sagorroy4778@gmail.com>
2026-05-16 23:17:51 +0600

    Banner added...

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Container from '../layouts/Container'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick"
+import BannerOne from '../../assets/images/banner-one.webp'
+import BannerTwo from '../../assets/images/banner-two.webp'
+import BannerThree from '../../assets/images/banner-three.webp'
+import BannerRsOne from '../../assets/images/bannerRs-one.webp'
+import BannerRsTwo from '../../assets/images/bannerRs-two.webp'
+
+const Banner = () => {
+  let settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 1500,
+    cssEase: "linear",
+  };
+  return (
+    <Container>
+      <div className='flex justify-between py-6'>
+        <div className='max-w-[872px] w-full mr-6'>
+          {/* left side slider start */}
+          <Slider {...settings}>
+            <div>
+              <img src={BannerOne} alt="Banner One" className='w-full rounded-xl' />
+            </div>
+            <div>
+              <img src={BannerTwo} alt="Banner One" className='w-full rounded-xl' />
+            </div>
+            <div>
+              <img src={BannerThree} alt="Banner One" className='w-full rounded-xl' />
+            </div>
+          </Slider>
+        </div>
+
+        {/* right side banner item start */}
+        <div className='flex flex-col'>
+          <div className='mb-6'>
+            <img src={BannerRsOne} alt="Banner Right One" />
+          </div>
+          <div>
+            <img src={BannerRsTwo} alt="Banner Right Two" />
+          </div>
+        </div>
+      </div>
+    </Container>
+
+  )
+}
+
+export default Banner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Container from '../components/layouts/Container'
+import Banner from '../components/Banner/Banner'
 
 const Home = () => {
   return (
     <>
+      <Banner />
       <Container>
         <div className='w-full bg-badgetwo h-[500px] text-center leading-[500px] text-9xl'>Home...</div>
       </Container>
