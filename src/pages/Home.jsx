ec4a4b510f15ef3516fe86247661ecb0168e6678
Sagor Roy <sagorroy4778@gmail.com>
2026-06-09 01:16:06 +0600

    Featured add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Container from '../components/layouts/Container'
 import Banner from '../components/Banner/Banner'
+import Featured from '../components/Featured'
 
 const Home = () => {
   return (
     <>
       <Banner />
-      <Container>
-        <div className='w-full bg-badgetwo h-[500px] text-center leading-[500px] text-9xl'>Home...</div>
-      </Container>
+      <Featured />
     </>
   )
 }
