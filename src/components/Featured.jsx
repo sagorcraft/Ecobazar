2508837ef4042ea2690102e7332f1950df524adc
Sagor Roy <sagorroy4778@gmail.com>
2026-06-16 13:03:28 +0600

    Some update today

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -7,51 +7,50 @@ import { GoPackage } from "react-icons/go";
 
 const Featured = () => {
   return (
-    <div className="pt-8 items-center">
-      <Container>
-        <div className='flex justify-between shadow-2xl  py-[40px] px-[40px]'>
-          <div className='flex items-center'>
-            <div>
-                <TbTruckDelivery  className='text-[40px] text-[#00B207] mr-[16px]' />
-            </div>
-            <div>
-                <h2 className='font-poppins font-semibold text-[16px] pb-[5px] text-[#1A1A1A]'>Free Shipping</h2>
-              <p className='font-poppins font-normal text-[15px] text-[#999999]'> Free shipping on all your order</p>
-            </div>
+    <Container>
+      <div className='flex mx-auto shadow-md  p-10'>
+        <div className='flex items-center w-[306px] gap-x-4 mx-auto'>
+          <div>
+              <TbTruckDelivery  className='text-[40px] text-[#00B207]' />
           </div>
-
-          <div className='flex items-center'>
-            <div>
-                <TfiHeadphoneAlt className='text-[40px] text-[#00B207] mr-[16px]' />
-            </div>
-            <div>
-                <h2 className='font-poppins font-semibold text-[16px] pb-[5px] text-[#1A1A1A]'>Customer Support 24/7</h2>
-              <p className='font-poppins font-normal text-[15px] text-[#999999]'>Instant access to Support</p>
-            </div>
+          <div>
+              <h2 className='font-poppins font-semibold text-[16px] text-[#1A1A1A]'>Free Shipping</h2>
+            <p className='font-poppins font-normal text-[15px] text-[#999999]'> Free shipping on all your order</p>
           </div>
-          
-          <div className='flex items-center'>
-            <div>
-                <RiShoppingBag4Line className='text-[40px] text-[#00B207] mr-[16px]' />
-            </div>
-            <div>
-                <h2 className='font-poppins font-semibold text-[16px] pb-[5px] text-[#1A1A1A]'>100% Secure Payment</h2>
-              <p className='font-poppins font-normal text-[15px] text-[#999999]'>We ensure your money is save</p>
-            </div>
-          </div>
-
-          <div className='flex items-center'>
-            <div>
-                <GoPackage className='text-[40px] text-[#00B207] mr-[16px]' />
-            </div>
-            <div>
-                <h2 className='font-poppins font-semibold text-[16px] pb-[5px] text-[#1A1A1A]'>Money-Back Guarantee</h2>
-              <p className='font-poppins font-normal text-[15px] text-[#999999]'>30 Days Money-Back Guarantee</p>
-            </div>
-          </div> 
         </div>
-      </Container>
-    </div>
+
+        <div className='flex items-center w-[306px] gap-x-4 mx-auto'>
+          <div>
+              <TfiHeadphoneAlt className='text-[40px] text-[#00B207]' />
+          </div>
+          <div>
+              <h2 className='font-poppins font-semibold text-[16px] text-[#1A1A1A]'>Customer Support 24/7</h2>
+            <p className='font-poppins font-normal text-[15px] text-[#999999]'>Instant access to Support</p>
+          </div>
+        </div>
+        
+        <div className='flex items-center w-[306px] gap-x-4 mx-auto'>
+          <div>
+              <RiShoppingBag4Line className='text-[40px] text-[#00B207]' />
+          </div>
+          <div>
+              <h2 className='font-poppins font-semibold text-[16px] text-[#1A1A1A]'>100% Secure Payment</h2>
+            <p className='font-poppins font-normal text-[15px] text-[#999999]'>We ensure your money is save</p>
+          </div>
+        </div>
+
+        <div className='flex items-center w-[306px] gap-x-4 mx-auto'>
+          <div>
+              <GoPackage className='text-[40px] text-[#00B207]' />
+          </div>
+          <div>
+              <h2 className='font-poppins font-semibold text-[16px] text-[#1A1A1A]'>Money-Back Guarantee</h2>
+            <p className='font-poppins font-normal text-[15px] text-[#999999]'>30 Days Money-Back Guarantee</p>
+          </div>
+        </div> 
+      </div>
+    </Container>
+
   )
 }
 
