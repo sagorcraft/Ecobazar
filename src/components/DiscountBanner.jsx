5fc910f85d19ebf8c778767204e2e2c7a9eedbc8
Sagor Roy <sagorroy4778@gmail.com>
2026-06-22 19:21:19 +0600

    DiscountBanner Update

--- src/components/DiscountBanner.jsx
+++ src/components/DiscountBanner.jsx
@@ -4,11 +4,13 @@ import DiscountImg from '../assets/images/discountbanner.webp'
 
 const DiscountBanner = () => {
   return (
-    <Container>
-      <div className='flex items-center justify-center py-15'>
-        <img src={DiscountImg} alt="Discount Banner" />
-      </div>
-    </Container>
+    <div className='px-4 py-15'>
+      <Container>
+        <div className='size-full group overflow-hidden rounded-[10px]'>
+          <img src={DiscountImg} alt="Discount Banner" className='w-full cursor-pointer group-hover:scale-[1.01] transition-all duration-300' />
+        </div>
+      </Container>
+    </div>
   )
 }
 
